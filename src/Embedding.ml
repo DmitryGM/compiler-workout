e007348901b9780fc4c0674cc3becefb8f1fc61b
DmitryGM <erropp@ya.ru>
2018-02-26 22:02:41 +0300

    fix Embedding.ml

--- src/Embedding.ml
+++ src/Embedding.ml
@@ -27,27 +27,14 @@ let ( != ) = binop "!="
 let ( && ) = binop "&&"
 let ( || ) = binop "!!"
 
-<<<<<<< HEAD
-(*
-=======
 let ( =:= ) x e = Stmt.Assign (x, e)
 let read  x = Stmt.Read x
 let write e = Stmt.Write e
 let (|>) x y = Stmt.Seq (x, y)
                              
->>>>>>> 3c0884cfa554ff5389a452d11065233a40863d0f
 (* Some predefined names for variables *)
 let x = !"x"
 let y = !"y"
 let z = !"z"
 let t = !"t"
 
-<<<<<<< HEAD
-(* Voila; comment this out before submitting the solution *)
-let _ =
-  List.iter (fun e -> Printf.printf "eval s (%s) = %d\n" (show(expr) e) (eval s e)) [x+y*z- !?3; t-z+y && x]
-
-*)
-=======
-                   
->>>>>>> 3c0884cfa554ff5389a452d11065233a40863d0f
